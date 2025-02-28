679d564710afa7de5d8570b29697243029ab7ffd
aarogyaojha <arogyaojha251@gmail.com>
2025-02-28 18:00:05 +0545

    added supabase and the domain

---
+++ lib/features/auth/domain/repository/auth_repository.dart
@@ -0,0 +1,15 @@
+import 'package:blog_app/core/error/failures.dart';
+import 'package:fpdart/fpdart.dart';
+
+abstract interface class AuthRepository {
+  Future<Either<Failure, String>> signUpWithEmailPassword({
+    required String email,
+    required String password,
+    required String name,
+  });
+
+    Future<Either<Failure, String>> loginWithEmailPassword({
+    required String email,
+    required String password,
+  });
+}
